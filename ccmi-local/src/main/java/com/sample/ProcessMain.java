96b5cf1913ea84f0369a972997a4fcbf414c9d15
Cade Friedenbach <cadenfriedenbach@gmail.com>
2015-08-20 11:19:37 -0400

    start of the day

--- ccmi-local/src/main/java/com/sample/ProcessMain.java
+++ ccmi-local/src/main/java/com/sample/ProcessMain.java
@@ -4,11 +4,6 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.jbpm.test.JBPMHelper;
-import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
@@ -21,21 +16,11 @@ import org.kie.api.builder.model.KieSessionModel;
 import org.kie.api.command.Command;
 import org.kie.api.command.KieCommands;
 import org.kie.api.event.rule.AfterMatchFiredEvent;
-import org.kie.api.event.rule.DebugAgendaEventListener;
-import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
 import org.kie.api.event.rule.DefaultAgendaEventListener;
 import org.kie.api.io.Resource;
 import org.kie.api.runtime.ExecutionResults;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.StatelessKieSession;
-import org.kie.api.runtime.manager.RuntimeEngine;
-import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
-import org.kie.api.runtime.manager.RuntimeManager;
-import org.kie.api.runtime.manager.RuntimeManagerFactory;
-import org.kie.api.task.TaskService;
-import org.kie.api.task.model.TaskSummary;
-import org.kie.api.event.rule.AgendaEventListener;
 
 public class ProcessMain {
 	
@@ -62,7 +47,6 @@ public class ProcessMain {
 		
 		ksession.addEventListener(new DefaultAgendaEventListener() {
 		    public void afterMatchFired(AfterMatchFiredEvent event) {
-		        // intentionally left blank
 		    	System.out.println(event.getMatch().getRule().getName());
 		    }
 		});
@@ -70,17 +54,6 @@ public class ProcessMain {
 		
 		ExecutionResults results = ksession.execute(commandFactory.newBatchExecution(commands));
 		System.out.println("results :" + results);
-	}
-
-	private static RuntimeManager createRuntimeManager(KieBase kbase) {
-		JBPMHelper.startH2Server();
-		JBPMHelper.setupDataSource();
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
-		RuntimeEnvironmentBuilder builder = RuntimeEnvironmentBuilder.Factory.get()
-			.newDefaultBuilder().entityManagerFactory(emf)
-			.knowledgeBase(kbase);
-		return RuntimeManagerFactory.Factory.get()
-			.newSingletonRuntimeManager(builder.get(), "com.sample:example:1.0");
 	}
 	
 	private static String createKieFileSystemAndBuild(KieServices ks){
@@ -93,7 +66,8 @@ public class ProcessMain {
 
         // Build builder
 		KieBuilder kBuilder = ks.newKieBuilder(kFile);
-		kBuilder.buildAll();
+		kBuilder.buildAll(); // where does this go to?
+		//and do what with the kie builder?
 		
 		if (kBuilder.getResults().hasMessages(Message.Level.ERROR)) {
 			for (Message message : kBuilder.getResults().getMessages(Message.Level.ERROR)) {
@@ -107,15 +81,16 @@ public class ProcessMain {
 		// Create ModuleDeploymentHelper
 		String time = Long.toString(System.currentTimeMillis());
 		FluentKieModuleDeploymentHelper helper = KieModuleDeploymentHelper.newFluentInstance();
-
+		  helper.addDependencies("com.rhc:ccmi-knowledge:0.0.1-SNAPSHOT");
 		// Create KModule
 		KieModuleModel kModuleModel = helper.getKieModuleModel();
+		
         KieBaseModel kBaseModel = kModuleModel.newKieBaseModel( "genkbase" )
-        		.setDefault(false);
+        		.setDefault(false).addInclude("addKbase");
         KieSessionModel kSessionModel = kBaseModel.newKieSessionModel( "GeneratedSession" )
         		.setType(KieSessionModel.KieSessionType.STATELESS)
         		.setDefault(false);
-        
+      
         // Create KJAR and deploy to maven
 		helper.setGroupId(kjarGroupId)
 			.setArtifactId(kjarArtifactId)
